67be2cf0c24e729ab7198b05a455003f83abc53b
Aadarsh-Mane <onlyaddy68@gmail.com>
2024-07-30 00:07:17 +0530

    added cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json()); // Parse JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-
+app.use(cors());
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.projectid,
